b72fa2a66d0be7d150c6773c880819e5e0397faf
nbuss848 <nbuss848@gmail.com>
2017-06-27 21:14:03 -0500

    commit

--- Chess/Main.cs
+++ Chess/Main.cs
@@ -24,7 +24,7 @@ namespace Chess
         {           
             GameBoard.Setup(GameType.Classic);
             // GameBoard.Print();       
-            //DrawBoard();
+            
             Draw();            
         }      
 
