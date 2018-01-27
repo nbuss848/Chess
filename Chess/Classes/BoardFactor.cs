f5fe05824e354c7c10b42b1a3e013bd136bc603c
nbuss848 <nbuss848@gmail.com>
2018-01-26 21:47:01 -0600

    hmm

---
+++ Chess/Classes/BoardFactor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Classes
+{
+    class BoardFactor
+    {
+        private object[,] _board;
+
+        public BoardFactor()
+        {
+            _board = new object[8,8];
+        }
+
+        //public _board { get; set; }
+    }
+}
